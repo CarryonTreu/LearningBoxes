a84b37ff0d7b47149a811e3f940a97f6a6577584
Johannes Treu <jotreu92@gmail.com>
2018-11-14 10:53:01 +0100

    got current state of project

---
+++ LearningBoxes/Model/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningBoxes.Model {
+    class Card {
+
+        private DateTime creationDate;
+        private DateTime lastEditDate;
+
+        private string cardName;
+        private int    cardId;
+        private string cardQuestion;
+        private string cardAwnser;
+
+        public Card(string cardName = "",
+                    string cardQuestion = "",
+                    string cardAwnser = "") {
+            //TODO generate new id
+            this.cardId = 0;
+
+            this.cardName = cardName;
+            this.cardQuestion = cardQuestion;
+            this.cardAwnser = cardAwnser;
+            DateTime current = new DateTime();
+            this.creationDate = current;
+            this.lastEditDate = current;
+        }
+
+        public void setCardName(string value) {
+            this.cardName = value;
+            this.lastEditDate = new DateTime();
+        }
+        public string getCardName() {
+            return this.cardName;
+        }
+    }
+}
